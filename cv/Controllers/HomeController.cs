4a87a51b5c965edb156116176a16197720f0d44d
Kristine Jedusa <47714515+KristineJedusa@users.noreply.github.com>
2019-02-22 18:00:53 +0200

    initial commit

---
+++ cv/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cv.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult PersonasDati()
+        {
+            return View();
+        }
+
+        public ActionResult DarbaPieredze()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Kontakti()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Izglitiba()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Prasmes()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Valodas()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Kursi()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult VaditajaAplieciba()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
